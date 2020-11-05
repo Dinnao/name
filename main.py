48d8ea97b0ffa72d95efeaa7fa57b85a41439cb5
Dinnao <73665013+Dinnao@users.noreply.github.com>
2020-11-05 22:44:08 +0200

    homework2

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
-#import this # дзен
-#import antigravity # комикс
-#import __hello__ # Hello world
-#from __future__ import braces # не выполниться
-######################
-print('\\a Bell(allert) \n\\b Backspace \n\\n New line')
-print('\\t Horizontal tab \n\\\ Backslash \ \n\\” Double quotation mark “')
-print("\\' Single quatation mark'")
-######################
-print("Введите два целых числа: ")
-x = int(input())
-y = int(input())
-print(x, y)
-print(x + y)
-print(x / y)
-print(x ** y)
-#не понял где можно посмотреть как сделать имя звёздочками
+import this
