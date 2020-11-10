079dfc8e65841ea96965af186ddf5015844c1888
Dinnao <73665013+Dinnao@users.noreply.github.com>
2020-11-10 23:02:05 +0200

    homework3.py

---
+++ homework3.py
@@ -0,0 +1,14 @@
+#new_list = list(input('Enter number: '))
+#print(int(new_list[0]) + int(new_list[1]) + int(new_list[2]))
+
+#some_numb = (input('Enter a float number: '))
+#new_list = some_numb.split('.')
+#print(new_list[1])
+
+#list_len = [10, 20, 30, 40, 50]
+#list_len.reverse()
+#print(list_len)
+
+#new_range = range(5, 155, 5)
+#for it in new_range:
+#    print(it, end=(','))
