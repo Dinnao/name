ce4b2c0d87e54fd8b20eb6c5d56a06e166f4b4be
Dinnao <73665013+Dinnao@users.noreply.github.com>
2020-11-15 12:53:34 +0200

    homework4.py

---
+++ homework4.py
@@ -0,0 +1,66 @@
+#x = int(input('Enter start km: '))
+#y = int(input('Enter end km: '))
+#day = 0
+#some = x / 10
+#while x != y:
+#    x += some
+#    day += 1
+#    print(day)
+
+#some_numb = [int(input('Enter any numbers. 0 = end: '))]
+#numb_of_some_numb = len(some_numb)
+#print(some_numb)
+#hile 0 not in some_numb:
+#    some_numb.append(int(input()))
+#    print(some_numb)
+#some_numb.remove(0)
+#print('len of numbers is: ' + str(len(some_numb)))
+#print('Sum of numbers is: ' + str(sum(some_numb)))
+#print(sum(some_numb)/len(some_numb))
+#max_digit = 0
+#num_mult = 1
+#num_count_1 = 0
+#num_count_2 = 0
+#for i, value in enumerate(some_numb):
+#    num_mult *= value
+#    if value > max_digit:
+#        max_digit = value
+#        max_digit_index = i
+#    if i % 2 == 0:
+#        num_count_2 += 1
+#    elif i % 2 != 0:
+#        num_count_1 += 1
+#print(num_mult)
+#print(num_count_2)
+#print(num_count_1)
+#print(max_digit, max_digit_index)
+
+
+#A = int(input('Enter an int number: '))
+#B = int(input('Enter second int number: '))
+#some = A
+#New_list = []
+#steps = 0
+#while some != B:
+#    some += 1
+#    steps += 1
+#while steps != 0:
+#    New_list.append(A)
+#    A += 1
+#    steps -= 1
+#New_list.append(B)
+#print(New_list)
+
+
+#n = int(input('Enter some number: '))
+#some_new = 0
+#stairs = []
+#while some_new != n:
+#    some_new += 1
+#    stairs.append(some_new)
+#print(stairs)
+
+
+
+
+
