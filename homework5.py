7da41920fb9cfa8184ba0c7a258dc466ea5e95bf
Dinnao <73665013+Dinnao@users.noreply.github.com>
2020-11-26 15:44:00 +0200

    homework6.py

---
+++ homework5.py
@@ -0,0 +1,30 @@
+#new_dict = {}
+#def comb_dict(keys, values):
+#    for k, v in zip(keys, values):
+#        new_dict[k] = v
+#comb_dict(keys=['Bitcoin', 'Ether', 'Ripple', 'Litecoin'] , values=['BTC', 'ETH', 'XRP', 'LTC'])
+#print(new_dict)
+
+#import datetime
+#import time
+#def count_doun(M):
+#    M = time.sleep(3)
+#@count_doun
+#def time_now(now):
+#    now = datetime.datetime.now()
+#    return print(now)
+#time_now(datetime.datetime.now())
+
+#New_dict = {}
+#new_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+#Key_list = []
+#len_of_list = len(new_list)
+#step = 0
+#while step != len_of_list:
+#   Key_list.append(step)
+#   step +=1
+#def comb_dict(keys, values):
+#    for k, v in zip(keys, values):
+#        New_dict[k] = v
+#comb_dict(keys=Key_list , values= new_list)
+#print(New_dict)
