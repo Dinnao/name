463b0d5ce9dd4ccbfd3188a66f250f5d38a5f43a
Dinnao <73665013+Dinnao@users.noreply.github.com>
2020-12-03 15:06:30 +0200

    homework1.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-#import this # дзен
-#import antigravity # комикс
-#import __hello__ # Hello world
-#from __future__ import braces # не выполниться
-######################
+import this # дзен
+import antigravity # комикс
+import __hello__ # Hello world
+from __future__ import braces # не выполниться
+
 print('\\a Bell(allert) \n\\b Backspace \n\\n New line')
 print('\\t Horizontal tab \n\\\ Backslash \ \n\\” Double quotation mark “')
 print("\\' Single quatation mark'")
-######################
+
 print("Введите два целых числа: ")
 x = int(input())
 y = int(input())
