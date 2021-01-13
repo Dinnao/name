d14226bd02fd30230a0646204a3eac414d34a869
Dinnao <73665013+Dinnao@users.noreply.github.com>
2021-01-13 19:28:39 +0200

    homework11

--- homework11/homework11.2.py
+++ homework11/homework11.2.py
@@ -1,6 +1,5 @@
-def secret_email(email):
-    email_s = email.replace(email[3:6], '***')
-    email_sc = email_s.replace(email[10:13], '***')
-    print(email_sc)
-secret_email('someemail@gmail.com')
-#не знаю сделал я правельно или нет, не понял как сдлеать чтоб оно скрвыало до и после '@'
+delimiter = '***@***'
+def secret_email(s_email):
+    email = s_email.index('@')
+    print(s_email[:email -5] + delimiter + s_email[email + 5:])
+secret_email(input('Please enter email: '))
